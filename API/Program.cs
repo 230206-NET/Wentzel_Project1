965bcfea19b88df9c3330a1c9a3f7ca6168862f2
nckwntzl <nckwntzl@gmail.com>
2023-03-03 09:11:12 -0500

    mvp

--- API/Program.cs
+++ API/Program.cs
@@ -20,10 +20,25 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "hello");
 
-app.MapGet("/pending", ([FromServices] DatabaseRepo service) => service.getPendingExpenses());
+app.MapGet("/pending", ([FromQuery] string? id, string? pass, [FromServices] DatabaseRepo service) => 
+pass.Equals(service.getPassByEmpId(id)) ? Results.Json(service.getPendingExpenses()) : Results.Unauthorized());
+
 
 app.MapGet("/expense", ([FromQuery] string? id, string? pass, [FromServices] DatabaseRepo service) =>  
 pass.Equals(service.getPassByEmpId(id)) ? Results.Json(service.getExpensesByEmpId(int.Parse(id))) : Results.Unauthorized());
 
+app.MapPost("/expense", ([FromQuery] string note, string pass, string empid, decimal value, [FromServices] DatabaseRepo service) =>
+pass.Equals(service.getPassByEmpId(empid)) ? Results.Created("/expense", service.putNewExpense(note,int.Parse(empid),value)) : Results.Unauthorized());
+
+app.MapPost("/register", ([FromQuery] string name, string pass, [FromServices] DatabaseRepo service) => {
+    return Results.Json(service.newEmployee(name,pass));
+});
+
+app.MapPost("/process", ([FromQuery] string status, int expid, string empid, string pass, [FromServices] DatabaseRepo service) => {
+    if(pass.Equals(service.getPassByEmpId(empid)))
+        return Results.Created("/expense", service.setExpenseStatus(expid, status));
+    return Results.Unauthorized();
+    });
 
 app.Run();
+
